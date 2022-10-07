b4898715be057a30e3915d4592a03cc2c787bb57
Zhadnov Alexfndr <a.zhadnov@mail.ru>
2022-10-07 20:14:09 +0300

    Version_1.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,33 @@
-const POPUP_ACTIVE = "popup_opened";
+const POPUP_ACTIVE = `popup_opened`;
 
-const openPopupBtn = document.querySelector(".profile__edit-button");
+const openEditBtn = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const closePopupBtn = popup.querySelector(".popup__button-close");
 const formElement = popup.querySelector(".popup__form");
-const savePopupBtn = popup.querySelector(".popup__button-save");
 
-let nameInput = popup.querySelector(".popup__input_name");
-let jobInput = popup.querySelector(".popup__input_profession");
+let nameInput = popup.querySelector(".popup__input_theme_name");
+let jobInput = popup.querySelector(".popup__input_theme_profession");
 let headingName = document.querySelector(".profile__name");
 let headingpProfession = document.querySelector(".profile__profession");
 
-function openPopup (){
+function openPopup(){
     popup.classList.add(POPUP_ACTIVE);
-    nameInput.value = headingName.textContent;
-    jobInput.value = headingpProfession.textContent;
 };
 
-function closePopup (){
+nameInput.value = headingName.textContent;
+jobInput.value = headingpProfession.textContent;
+
+function closePopup(){
     popup.classList.remove(POPUP_ACTIVE);
 };
 
-function formSubmitHandler (evt){
+function formSubmitHandler(evt){
     evt.preventDefault();
     headingName.textContent = nameInput.value;
     headingpProfession.textContent = jobInput.value;
+    popup.classList.remove(POPUP_ACTIVE);
 };
 
-openPopupBtn.addEventListener("click", openPopup);
+openEditBtn.addEventListener("click", openPopup);
 closePopupBtn.addEventListener("click", closePopup);
 formElement.addEventListener("submit", formSubmitHandler);
-savePopupBtn.addEventListener("click", closePopup);
